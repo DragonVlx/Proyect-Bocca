57cfc50846f61092ffc6bea4751a5c541813b00a
Johan Velandia <77521649+DragonVlx@users.noreply.github.com>
2021-10-03 21:42:44 -0500

    Fixed Mobile menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector('.hamburguer');
 const nav = document.querySelector('#mobile-menu');
+const closeHamburguer = document.querySelector("#mobile-menu")
 
 hamburger.addEventListener('click', () => {
     console.log("open");
     nav.classList.toggle('show');
 });
+
+
+closeHamburguer.addEventListener('click', () => {
+    closeHamburguer.classList.remove('show');
+})
